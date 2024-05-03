d31d7a2f220cd375aff0282136d1bb21cea588dc
Eyoel Meles <=>
2024-05-03 12:06:14 +0800

    :shamrock: initial theme, add sabi-pay

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,30 @@
 import { Text, View, StyleSheet } from "react-native";
 import { APP_LIST } from "../apps";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { Link } from "expo-router";
+import { Link, SplashScreen } from "expo-router";
+import { useFonts } from "expo-font";
+import { useCallback } from "react";
+import { WALLET_FONTS } from "./crypto_wallet/(constant)/theme";
 
 export default () => {
+  const [fontsLoaded, fontError] = useFonts({
+    "Inter-Black": require("../assets/fonts/Inter-Black.ttf"),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded || fontError) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, fontError]);
+
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
+
   return (
-    <View style={styles.constainer}>
+    <View style={styles.constainer} onLayout={onLayoutRootView}>
       <SafeAreaView>
+        <Text style={[WALLET_FONTS.h1]}>APPS LIST</Text>
         {APP_LIST.map((app, index) => (
           <Link key={index} href={app.route}>
             <View style={styles.app_list}>
